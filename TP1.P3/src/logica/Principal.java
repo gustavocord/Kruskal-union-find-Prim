d4e0eac5fd3d473f1eafc081085a6ae06f1acf9f
gustavo cordoba <cordobagustavoj@gmail.com>
2021-09-24 23:27:30 -0300

    clase principal

---
+++ TP1.P3/src/logica/Principal.java
@@ -0,0 +1,20 @@
+package logica;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("probando kruskal");
+		Kruskal kruskal = new Kruskal(9);
+		kruskal.g.setArista(2, 3, 350);
+		kruskal.g.setArista(0, 1, 250);
+		kruskal.g.setArista(1, 2, 120);
+		
+		kruskal.agregarArista(new Arista(1,2,120));
+		Arista minima = kruskal.dameMinimaNoMarcada();
+		System.out.println(new Arista(0, 1, 250).equals(minima));
+	}
+
+}
