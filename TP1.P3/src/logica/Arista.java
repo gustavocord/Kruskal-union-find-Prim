fd22794822339035b6e94a7523883e86142f3ffe
gustavo cordoba <cordobagustavoj@gmail.com>
2021-10-13 02:25:24 -0300

    refactor code

--- TP1.P3/src/logica/Arista.java
+++ TP1.P3/src/logica/Arista.java
@@ -2,21 +2,21 @@ package logica;
 
 
 public class Arista implements Comparable<Arista>{
-	private int verticeI;
-	private int verticeJ;
-	private int peso;
+	private int _verticeI;
+	private int _verticeJ;
+	private int _peso;
 	public Arista(int verticeI, int verticeJ){
 		if(verticeI<0 || verticeJ<0 || (verticeI==verticeJ)) {
 			throw new IllegalArgumentException("La Arista: (" + verticeI + "," + verticeJ + ") no es valida");
 		}
-		this.verticeI= verticeI;
-		this.verticeJ= verticeJ;
-		this.peso= -1;//indica que no esta definida
+		this._verticeI= verticeI;
+		this._verticeJ= verticeJ;
+		this._peso= -1;//indica que no esta definida
 	}
 	public Arista(int verticeI, int verticeJ,int peso){
 		this(verticeI,verticeJ);
 		verificarPeso(peso);
-		this.peso= peso;
+		this._peso= peso;
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -29,45 +29,45 @@ public class Arista implements Comparable<Arista>{
 		}
 		Arista objConvertido= (Arista) obj;
 
-		boolean valor= (this.verticeI == objConvertido.verticeI) || (this.verticeI == objConvertido.verticeJ);
-		boolean valor2= (this.verticeJ == objConvertido.verticeI) || (this.verticeJ == objConvertido.verticeJ);
+		boolean valor= (this._verticeI == objConvertido._verticeI) || (this._verticeI == objConvertido._verticeJ);
+		boolean valor2= (this._verticeJ == objConvertido._verticeI) || (this._verticeJ == objConvertido._verticeJ);
 
 		return (valor && valor2);
 	}
 
 	@Override
 	public int hashCode() {
-		int mayor= this.verticeI > this.verticeJ? this.verticeI : this.verticeJ;
-		int menor= this.verticeI < this.verticeJ? this.verticeI : this.verticeJ;
+		int mayor= this._verticeI > this._verticeJ? this._verticeI : this._verticeJ;
+		int menor= this._verticeI < this._verticeJ? this._verticeI : this._verticeJ;
 		return (Integer.toString(menor) + "," + Integer.toString(mayor)).hashCode(); //de esta forma se distingue mejor
 	};
 	@Override
 	public String toString() {
-		return "(" + this.verticeI + "," + this.verticeJ + ")";
+		return "(" + this._verticeI + "," + this._verticeJ + ")";
 	}
 	public int getVerticeI() {
-		return verticeI;
+		return _verticeI;
 	}
 	public void setVerticeI(int verticeI) {
-		this.verticeI = verticeI;
+		this._verticeI = verticeI;
 	}
 	public int getVerticeJ() {
-		return verticeJ;
+		return _verticeJ;
 	}
 	public void setVerticeJ(int verticeJ) {
-		this.verticeJ = verticeJ;
+		this._verticeJ = verticeJ;
 	}
 
 	@Override
 	public int compareTo(Arista o) { //compara el peso
-		return this.peso - o.getPeso() ; //orden ascendente
+		return this._peso - o.getPeso() ; //orden ascendente
 
 	}
 	public int getPeso() {
-		return peso;
+		return _peso;
 	}
 	public void setPeso(int peso) {
-		this.peso = peso;
+		this._peso = peso;
 	}
 	private void verificarPeso(int peso) {
 		if(peso < 0) {
