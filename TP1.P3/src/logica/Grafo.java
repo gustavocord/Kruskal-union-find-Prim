84f108d4b8ff360ebc52a4471e6fa1d395b2a4f0
gustavo cordoba <cordobagustavoj@gmail.com>
2021-10-13 02:26:22 -0300

    refactor code

--- TP1.P3/src/logica/Grafo.java
+++ TP1.P3/src/logica/Grafo.java
@@ -6,19 +6,19 @@ import java.util.Random;
 import java.util.Set;
 
 public class Grafo {
-	private ArrayList<Set<Integer>> vecinos;
-	protected Set <Arista> aristas;
-	protected int cantidadDeVertices; 
-	protected int limiteDeAristas;
+	private ArrayList<Set<Integer>> _vecinos;
+	protected Set <Arista> _aristas;
+	protected int _cantidadDeVertices; 
+	protected int _limiteDeAristas;
 
 	public Grafo(int cantidadDeVertices){
-		this.vecinos = new ArrayList<Set<Integer>>(cantidadDeVertices);
+		this._vecinos = new ArrayList<Set<Integer>>(cantidadDeVertices);
 		for (int i = 0; i < cantidadDeVertices; i++) {
-			this.vecinos.add(new HashSet<Integer>());
+			this._vecinos.add(new HashSet<Integer>());
 		}
-		this.cantidadDeVertices = cantidadDeVertices;
-		this.aristas= new HashSet<Arista>();
-		this.limiteDeAristas= (cantidadDeVertices * (cantidadDeVertices-1) ) / 2;
+		this._cantidadDeVertices = cantidadDeVertices;
+		this._aristas= new HashSet<Arista>();
+		this._limiteDeAristas= (cantidadDeVertices * (cantidadDeVertices-1) ) / 2;
 	}
 	public Grafo(int cantidadDeVertices, int cantDeAristas) {
 
@@ -35,9 +35,9 @@ public class Grafo {
 
 	public void agregarArista(int verticeI, int verticeJ){	//O(1)
 		verificarArista(verticeI, verticeJ,"agregar");
-		this.vecinos.get(verticeI).add(verticeJ);
-		this.vecinos.get(verticeJ).add(verticeI);
-		this.aristas.add(new Arista(verticeI,verticeJ));
+		this._vecinos.get(verticeI).add(verticeJ);
+		this._vecinos.get(verticeJ).add(verticeI);
+		this._aristas.add(new Arista(verticeI,verticeJ));
 	}
 
 	public void eliminarArista(int verticeI, int verticeJ){	//O(1)
@@ -45,24 +45,24 @@ public class Grafo {
 		if(!existeArista(verticeI, verticeJ)) {
 			throw new IllegalArgumentException("Se intento eliminar una arista que no existe: "+ verticeI +"/" + verticeJ);
 		}
-		this.vecinos.get(verticeI).remove(verticeJ);
-		this.vecinos.get(verticeJ).remove(verticeI);
-		this.aristas.remove(new Arista(verticeI,verticeJ));
+		this._vecinos.get(verticeI).remove(verticeJ);
+		this._vecinos.get(verticeJ).remove(verticeI);
+		this._aristas.remove(new Arista(verticeI,verticeJ));
 
 	}
 
 	public boolean existeArista(int verticeI, int verticeJ){//O(1)
-		return this.vecinos.get(verticeI).contains(verticeJ);
+		return this._vecinos.get(verticeI).contains(verticeJ);
 	}
 
 	public Set<Integer> vecinos(int vertice){	//O(1)
 		verificarVertice(vertice, "obtener los vecinos de un vertice");
-		return this.vecinos.get(vertice);
+		return this._vecinos.get(vertice);
 	}
 
 	public int grado(int vertice){ 
 		verificarVertice(vertice, "consultar");
-		return vecinos.get(vertice).size();
+		return _vecinos.get(vertice).size();
 	}
 
 	//BFS
@@ -74,31 +74,31 @@ public class Grafo {
 		if(!esConexo()) {
 			return false;
 		}
-		if(this.aristas.size() != (this.cantidadDeVertices -1) ) {
+		if(this._aristas.size() != (this._cantidadDeVertices -1) ) {
 			return false;
 		}
 		return true;
 	}
 
 	private void grafoAleatorioConCantDeAristas(int cantidadDeAristas) {
-		if(cantidadDeAristas<0 || cantidadDeAristas>this.limiteDeAristas) {
-			throw new IllegalArgumentException("La cantidad de aristas debe ser: [0," + (this.limiteDeAristas) + "]");
+		if(cantidadDeAristas<0 || cantidadDeAristas>this._limiteDeAristas) {
+			throw new IllegalArgumentException("La cantidad de aristas debe ser: [0," + (this._limiteDeAristas) + "]");
 		}
 		while(this.getAristas().size()<cantidadDeAristas) {
-			int iAleatorio= numeroRandomEntre(0,this.cantidadDeVertices);
-			int jAleatorio= numeroRandomEntre(0,this.cantidadDeVertices);
+			int iAleatorio= numeroRandomEntre(0,this._cantidadDeVertices);
+			int jAleatorio= numeroRandomEntre(0,this._cantidadDeVertices);
 			if(iAleatorio!=jAleatorio) {
 				this.agregarArista(iAleatorio, jAleatorio);
 			}
 		}
 	}
 	private void grafoAleatorioConexo() {
-		int[] arrayVertices= new int[this.cantidadDeVertices];
-		for(int i=0; i<this.cantidadDeVertices;i++) {
+		int[] arrayVertices= new int[this._cantidadDeVertices];
+		for(int i=0; i<this._cantidadDeVertices;i++) {
 			arrayVertices[i]=i;
 		}
-		for(int i=0; i<this.cantidadDeVertices-1 ;i++) {
-			int indiceJAleatorio= numeroRandomEntre(i+1,this.cantidadDeVertices);
+		for(int i=0; i<this._cantidadDeVertices-1 ;i++) {
+			int indiceJAleatorio= numeroRandomEntre(i+1,this._cantidadDeVertices);
 			int elemI= arrayVertices[i];
 			int elemJAleatorio= arrayVertices[indiceJAleatorio];
 			this.agregarArista(elemI, elemJAleatorio);
@@ -120,7 +120,7 @@ public class Grafo {
 	}
 
 	protected void verificarVertice(int vertice, String accion) {
-		if (vertice < 0 || vertice >= cantidadDeVertices) {
+		if (vertice < 0 || vertice >= _cantidadDeVertices) {
 			throw new IllegalArgumentException("Se intento "+ accion +" con valores fuera de rango: "+ vertice);
 		}
 	}
@@ -133,9 +133,9 @@ public class Grafo {
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder("|Grafo: \n");
-		for (int i = 0; i < this.cantidadDeVertices; i++) {
+		for (int i = 0; i < this._cantidadDeVertices; i++) {
 			sb.append("|" + i + ": [ ");
-			for(Integer elem : this.vecinos.get(i)) {
+			for(Integer elem : this._vecinos.get(i)) {
 				sb.append("[" + elem.toString() + "]");
 			}
 			sb.append(" ] \n");
@@ -143,28 +143,28 @@ public class Grafo {
 		return sb.toString();
 	}
 	public ArrayList<Set<Integer>> getVecinos() {
-		return this.vecinos;
+		return this._vecinos;
 	}
 	public Set<Arista> getAristas() {
-		return this.aristas;
+		return this._aristas;
 	}
 	public int getCantidadDeVertices() {
-		return this.cantidadDeVertices;
+		return this._cantidadDeVertices;
 	}
 	public int getLimiteDeAristas() {
-		return limiteDeAristas;
+		return _limiteDeAristas;
 	}
 	public void setLimiteDeAristas(int limiteDeAristas) {
-		this.limiteDeAristas = limiteDeAristas;
+		this._limiteDeAristas = limiteDeAristas;
 	}
 	public void setVecinos(ArrayList<Set<Integer>> vecinos) {
-		this.vecinos = vecinos;
+		this._vecinos = vecinos;
 	}
 	public void setAristas(Set<Arista> aristas) {
-		this.aristas = aristas;
+		this._aristas = aristas;
 	}
 	public void setCantidadDeVertices(int cantidadDeVertices) {
-		this.cantidadDeVertices = cantidadDeVertices;
+		this._cantidadDeVertices = cantidadDeVertices;
 	}	
 }
 
